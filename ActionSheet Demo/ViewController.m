372aa9ce6a416beac3d60426ea89414e5dcf10d1
tsunglintsai <tsunglin.tsai@gmail.com>
2013-03-24 23:11:33 -0700

    Complete implementation

--- ActionSheet Demo/ViewController.m
+++ ActionSheet Demo/ViewController.m
@@ -8,8 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+#define OTHER_BUTTON_1_TITLE @"Other Button 1"
+#define OTHER_BUTTON_2_TITLE @"Other Button 1"
 
+@interface ViewController ()<UIActionSheetDelegate>
+@property (nonatomic,weak) UIActionSheet *actionSheet;
 @end
 
 @implementation ViewController
@@ -20,10 +23,35 @@
 	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+- (IBAction)actionBtnClicked:(id)sender {
+    if(!self.actionSheet){
+        UIActionSheet *actionSheet = [[UIActionSheet alloc]initWithTitle:@"title"
+                                                                delegate:self cancelButtonTitle:@"Cancel"
+                                                  destructiveButtonTitle:@"Delete"
+                                                       otherButtonTitles:OTHER_BUTTON_1_TITLE,OTHER_BUTTON_2_TITLE, nil];
+        [actionSheet showFromBarButtonItem:sender animated:YES];
+        self.actionSheet = actionSheet;
+    }
 }
+
+#pragma mark - actionsheet delegate
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex{
+    if(buttonIndex == self.actionSheet.destructiveButtonIndex){
+        NSLog(@"destuctivebutton clicked");
+    }else if(buttonIndex == self.actionSheet.cancelButtonIndex){
+        NSLog(@"cancel clicked");
+    }else{
+        NSString *choice = [actionSheet buttonTitleAtIndex:buttonIndex];
+        if([choice isEqualToString:OTHER_BUTTON_1_TITLE]){
+            NSLog(@"%@ clicked",OTHER_BUTTON_1_TITLE);
+        }else if([choice isEqualToString:OTHER_BUTTON_2_TITLE]){
+            NSLog(@"%@ clicked",OTHER_BUTTON_2_TITLE);
+        }
+        NSLog(@"clicked button at index:%i",buttonIndex);
+        
+    }
+}
+
 
 @end
