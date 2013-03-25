7f0a2513974314f881ef9e669aa5753039abdad2
tsunglintsai <tsunglin.tsai@gmail.com>
2013-03-24 22:38:21 -0700

    Initial Commit

---
+++ ActionSheet Demo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ActionSheet Demo
+//
+//  Created by Daniela on 3/24/13.
+//  Copyright (c) 2013 Pyrogusto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
